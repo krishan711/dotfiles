06b882a1e4e4d5903c0763d24986d5973744e31a
Krishan Patel <krishan711@gmail.com>
2021-12-10 15:20:04 +0000

    Feature: add krishali backup folder

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 1.0.0"
+
+  backend "s3" {
+    # aws s3 mb s3://kiba-infra-krishali
+    key = "tf-state.json"
+    region = "eu-west-1"
+    bucket = "kiba-infra-krishali"
+    profile = "kiba"
+    encrypt = true
+  }
+  required_providers {
+    aws = {
+      version = "3.32.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  profile = "kiba"
+}
+
+locals {
+  project = "krishali"
+}
