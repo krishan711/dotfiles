262ca5afaf621a5895881106b1440dea9c324250
Krishan Patel <krishan711@gmail.com>
2026-06-17 13:51:04 +0200

    small updates

--- envrc-template.sh
+++ envrc-template.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+
 expected_env=".env"
 expected_path="$expected_env/bin/activate"
 if [ ! -d "$expected_env" ]; then
     echo "Making $expected_env..."
-    python3 -m venv $expected_env
+    python3.14 -m venv $expected_env
     source $expected_path
     pip install --upgrade pip
     deactivate
 fi
 echo "Loading $expected_path..."
 source $expected_path
+export UV_PROJECT_ENVIRONMENT=$VIRTUAL_ENV
 
 unset PS1
